02206ad0e878243e2018d1605c6f6f0f1cee8f7c
DavidTorresCe <141770411+DavidTorresCe@users.noreply.github.com>
2023-11-06 19:52:53 -0800

    https://github.com/DavidTorresCe/prueba.git

--- preprocesamiento_tarea.py
+++ preprocesamiento_tarea.py
@@ -2,15 +2,14 @@ import pandas as pd
 
 datos = pd.read_csv("datasets/surveys.csv")
 
-
 def porcentaje_nulos(value):
     nulos = value.isnull()
     porcentaje = nulos.sum()/len(value)
     print("el porcentaje de nulos de cada columna es: \n",porcentaje)
 
 def duplicados(value):
-    eliminar_duplicados = value.drop_duplicates(subset=["sex", "weight"])
-    print("La cantidad de valores duplicados es de: ",len(eliminar_duplicados))
+    duplicados = value.drop_duplicates(subset=["sex", "weight"])
+    print("La cantidad de valores duplicados es de: ",len(duplicados))
 
 def nulos_eliminados(value, porcentaje):
     if porcentaje < 0 or porcentaje > 1:
@@ -23,16 +22,40 @@ def nulos_eliminados(value, porcentaje):
         if value[i].isna().sum() >= nulos_permitidos:
             eliminados.append(i)
 
-    df = datos.drop(eliminados, axis = 1)
+    df = value.drop(eliminados, axis = 1)
     print("Las columnas eliminadas han sido: ", eliminados)
     print("Cantidad de columnas eliminadas: ",len(eliminados))
     print(df)
 
-def sustitucion(value, lista, cadena):
-    pass
+
+def sustitucion(dataframe, columnas, metodo):
+    if metodo not in ['mean', 'bfill', 'ffill']:
+        raise ValueError("El método debe ser 'mean', 'bfill' o 'ffill'")
+
+    if metodo == 'mean':
+        dataframe[columnas] = dataframe[columnas].fillna(dataframe[columnas].mean())
+    elif metodo == 'bfill':
+        dataframe[columnas] = dataframe[columnas].bfill()
+    elif metodo == 'ffill':
+        dataframe[columnas] = dataframe[columnas].ffill()
+
+    return dataframe
+
+def eliminar_duplicados(value):
+    eliminar_duplicados = value.drop_duplicates(subset=["sex", "weight"])
+    print("La cantidad de valores duplicados es de: ", len(eliminar_duplicados))
+
+
 
 
 porcentaje_nulos(datos)
 duplicados(datos)
-nulos_eliminados(datos, 0.031)
+nulos_eliminados(datos, 0.021)
+
+lista = ["hindfoot_length", "weight"]
+sustitucion(datos, lista, "mean")
+
+eliminar_duplicados(datos)
+
+
 
